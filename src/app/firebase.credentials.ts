ba9a9e9b9c30a7dffb88abe708783fc1f65d983f
sarthvader <sarthak.bhatnagar@uconn.edu>
2018-03-31 19:31:54 -0400

    firebase credentials

---
+++ src/app/firebase.credentials.ts
@@ -0,0 +1,8 @@
+export const FIREBASE_CREDENTIALS = {
+  apiKey: "AIzaSyChE6lWPZXVflosNJr5aOQ0Frf-jBbYy68",
+  authDomain: "mosum-2d784.firebaseapp.com",
+  databaseURL: "https://mosum-2d784.firebaseio.com",
+  projectId: "mosum-2d784",
+  storageBucket: "mosum-2d784.appspot.com",
+  messagingSenderId: "876357155846"
+}
